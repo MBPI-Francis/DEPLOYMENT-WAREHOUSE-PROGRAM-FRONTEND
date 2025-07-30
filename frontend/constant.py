31654007a1dd555273235634995ac3d810198759
MBPI-Francis <mbpi.francis@gmail.com>
2025-07-30 15:18:32 +0800

    07/30/2025

--- frontend/constant.py
+++ frontend/constant.py
@@ -1,5 +1,10 @@
 from enum import Enum
 
 
+
 class FormConstant(Enum):
+
+    """ Don't change the string. It is use for decrypting the SOH excel file
+        when the user Generates a SOH Excel File
+    """
     EXCEL_SOH_PASSWORD = "maranatha101"
